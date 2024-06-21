72f044d27f46b60e7717433279a7915e60bfa3d3
Maria Dina Savassini <83993951+mdsavassini@users.noreply.github.com>
2024-06-21 08:06:21 -0300

    Add files via upload

---
+++ formulario/index.php
@@ -0,0 +1,17 @@
+<?php  
+
+    $hostname = "localhost";
+    $bancodedados = "formulario";
+    $usuario = "root";
+    $senha = "";
+
+    $mysqli = new  mysqli($hostname, $bancodedados, $usuario, $senha);
+
+    //if($mysqli->connect_errno)
+    //{
+    //    echo"Erro";
+    //}
+    //else{
+    //    echo"conexao efetuada com sucesso";
+    //}
+?>
